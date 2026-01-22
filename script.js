b39a1bf7e12de75dc354e55f0b8cf84786a44031
redmas-mods <olivierfightlemal@gmail.com>
2026-01-22 15:56:28 +0100

    u

--- script.js
+++
@@ -1,8 +0,0 @@
-// Un exemple basique : afficher une alerte quand on clique sur un bouton
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll("a").forEach(function(link) {
-        link.addEventListener("click", function() {
-            alert("Merci de télécharger le mod !");
-        });
-    });
-});
