c3306b966adaa4c9e148ee1e05ed4d71d615befb
redmas-mods <olivierfightlemal@gmail.com>
2026-01-23 12:47:25 +0100

    u

---
+++ info/inc/th-option/sidebar.php
@@ -0,0 +1,33 @@
+<div class="sidebar-section">
+            <div class="section">
+            <h3 class="hndle ui-sortable-handle">
+                <span><?php _e('Oneline Premium Theme','oneline-lite'); ?> </span>
+            </h3>
+            <p>
+            <b><?php _e('Pre-made premium templates','oneline-lite'); ?> </b><br>
+            <?php _e('Oneline Pro is one of the most popular eCommerce theme on WordPress.','oneline-lite'); ?> </p>
+                <center><a class="button ztabtn" target="_blank" href="https://themehunk.com/product/oneline-single-page-wordpress-theme/"><?php _e('Upgrade To Oneline Premium','oneline-lite'); ?> </a></center>
+            </div>
+            <hr>
+            <div class="section">
+                <h3><?php _e('Why Upgrade?','oneline-lite'); ?></h3>
+                <p><?php _e('Upgrading to Big Store Premium you will unlock dozen of unique features that will take your website to the next level. See the Comparison table for more details.','oneline-lite'); ?> </p>
+                 <center><a class="button button-primary" target="_blank" href="https://themehunk.com/product/oneline-single-page-wordpress-theme/#tabfix"><?php _e('View Oneline Premium Demo','oneline-lite'); ?> </a></center>
+
+            </div>
+            <hr>
+
+            <div class="section">
+                <h3><?php _e('Leave us a review','oneline-lite'); ?></h3>
+                <p><?php _e('We would love to hear your feedback.','oneline-lite'); ?> </p>
+                 <a href="https://wordpress.org/support/theme/oneline-lite/reviews/?rate=5#new-post" target="_blank" class="sidebar-link"><?php _e('Submit review','oneline-lite'); ?></a>
+
+            </div>
+            <hr>
+
+            <div class="section">
+                <h3><?php _e('Support','oneline-lite'); ?> </h3>
+                <p><?php _e('Have a question, we are happy to help! Get in touch with our support team.','oneline-lite'); ?></p>
+                <a href="https://themehunk.com/contact-us/" target="_blank" class="sidebar-link"><?php _e('Submit a Ticket','oneline-lite'); ?></a>
+            </div>
+        </div>
